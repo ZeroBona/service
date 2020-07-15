386c23ac1efc278b61e774795050918367cb3943
wangqiang <wang.qiang@dayang.com.cn>
2020-07-15 22:02:38 +0800

    增加接口逻辑

--- app/controller/front/home.js
+++ app/controller/front/home.js
@@ -3,7 +3,8 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    let articleList = await this.app.mysql.select('article');
+    let sql = 'Select * FROM article where type = "read"'
+    let articleList = await this.app.mysql.query(sql);
     ctx.body = articleList;
   }
 }
